201cbb246df68e124aad7deaaab3a6c2f10c4d46
wtk <2289956060@qq.com>
2026-01-18 22:55:08 +0800

    first commit

--- src/main/java/com/example/demo/Service/Impl/AnswerServiceImpl.java
+++ src/main/java/com/example/demo/Service/Impl/AnswerServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.demo.Service.Impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.example.demo.DTO.response.AnswerVO;
 import com.example.demo.Service.AnswerService;
@@ -13,6 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -37,9 +42,36 @@ public class AnswerServiceImpl implements AnswerService {
         return convertToVO(answer,userId);
     }
 
+    /**
+     * 未实现分页查询
+     * @param questionId
+     * @param currentUserId
+     * @return
+     */
     @Override
     public List<AnswerVO> getAnswersByQuestionId(Long questionId, Long currentUserId) {
-        return null;
+//        answerMapper.selectList(new QueryWrapper<Answer>()
+//                        .eq("question_id",questionId)
+//                        .eq("is_deleted",false)
+//                        .orderByDesc("created_at")
+//                );
+
+        List<Answer> list = answerMapper.selectList(new LambdaQueryWrapper<Answer>()
+                .eq(Answer::getQuestionId,questionId)
+                .eq(Answer::getIsDeleted,false)
+                .orderByDesc(Answer::getCreatedAt));
+        List<AnswerVO> answerVOList = new ArrayList<>();
+//        for (int i = 0;i<list.size();i++) {
+//            AnswerVO answerVO = new AnswerVO();
+//            BeanUtil.copyProperties(list.get(i),answerVO);
+//            answerVOList.add(answerVO);
+//        }
+        for (Answer answer : list) {
+            AnswerVO answerVO = new AnswerVO();
+            BeanUtil.copyProperties(answer,answerVO);
+            answerVOList.add(answerVO);
+        }
+        return answerVOList;
     }
 
     @Override
